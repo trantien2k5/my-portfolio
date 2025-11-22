21446b6bec959b8c738485c32cb38b22bd9fc945
Trần Tiến <112411318+trantien2k5@users.noreply.github.com>
2025-11-22 13:59:55 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Trần Tiến đã load xong!');
+    alert('Chào mừng bạn đến với portfolio của Trần Tiến trên GitHub Pages!');
+});
 
